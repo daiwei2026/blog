3491b03e398359c40e35e8f73fb47a3e45e8f4f2
daiwei2026 <17206196706@139.com>
2026-02-23 15:59:32 +0800

    blog version 1.0

--- detail.php
+++ detail.php
@@ -34,5 +34,37 @@
 		echo "<p>$row[2]</p><p>$row[3]</p><p>$row[4]</p><a href=blogBefore.php?id=" . $row[0] . ">modify</a><a href=delete.php?id=" . $row[0] . ">delete</a><br>";
 		?>
 	</div>
+	<div id="nav">
+		<?php
+		$servername = 'localhost';
+		$username = 'root';
+		$password = '@Passw0rd';
+		$dbname = 'blog';
+		$id = $_GET["id"];
+		// 创建连接
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+			die('连接失败: ' . $conn->connect_error);
+		} 
+		if(!$_GET["category"]){
+			$sql = "SELECT * FROM category LIMIT 1;";
+			$result = $conn->query($sql);
+			$category = $result->fetch_row()[0];
+		}else{
+			$category = $_GET["category"];
+		}
+		$sql = 'SELECT * FROM blog WHERE category=' . $category . ' AND id < ' . $id . ' ORDER BY id DESC LIMIT 1' ;
+		$result = $conn->query($sql);
+		$row = $result->fetch_row();
+		if($row)
+			echo "<a href='detail.php?id=" . $row[0] . "'>previews</a>";
+		$sql = 'SELECT * FROM blog WHERE category=' . $category . ' AND id > ' . $id . ' ORDER BY id ASC LIMIT 1' ;
+		$result = $conn->query($sql);
+		$row = $result->fetch_row();
+		if($row)
+			echo "<a href='detail.php?id=" . $row[0] . "'>next</a>";
+		?>
+	</div>
 </body>
 </html>
--- index-1.php
+++ index-1.php
@@ -18,13 +18,6 @@
 		$username = 'root';
 		$password = '@Passw0rd';
 		$dbname = 'blog';
-		if(!$_GET["category"]){
-			$sql = "SELECT * FROM category LIMIT 1;";
-			$result = $conn->query($sql);
-			$category = $result->fetch_row()[0];
-		}else{
-			$category = $_GET["category"];
-		}
 		 
 		// 创建连接
 		$conn = new mysqli($servername, $username, $password, $dbname);
@@ -32,8 +25,18 @@
 		if ($conn->connect_error) {
 			die('连接失败: ' . $conn->connect_error);
 		} 
-		 
-		$sql = 'SELECT * FROM blog WHERE category=' . $category;
+		if(!$_GET["category"]){
+			$sql = "SELECT * FROM category LIMIT 1;";
+			$result = $conn->query($sql);
+			$category = $result->fetch_row()[0];
+		}else{
+			$category = $_GET["category"];
+		}
+		
+		if($_GET["current"])
+			$start = ($_GET["current"] - 1) * 10;
+		else $start = 0;
+		$sql = 'SELECT * FROM blog WHERE category=' . $category . ' LIMIT ' . $start . ', 10';
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 			// 输出数据
@@ -44,6 +47,50 @@
 			echo "0 结果";
 		}
 		echo "<a href='newBlog.php?category=" . $category . "'>newBlog</a>";
+		
+		?>
+	</div>
+	<div id="nav">
+		<?php
+		$servername = 'localhost';
+		$username = 'root';
+		$password = '@Passw0rd';
+		$dbname = 'blog';
+		// 创建连接
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+			die('连接失败: ' . $conn->connect_error);
+		} 
+		if(!$_GET["category"]){
+			$sql = "SELECT * FROM category LIMIT 1;";
+			$result = $conn->query($sql);
+			$category = $result->fetch_row()[0];
+		}else{
+			$category = $_GET["category"];
+		}
+		$sql = 'SELECT count(*) FROM blog WHERE category=' . $category;
+		$result = $conn->query($sql);
+		$row = $result->fetch_row();
+		if($row[0] == 0)
+			$pages = 0;
+		else if($row[0] % 10 == 0)
+			$pages = $row[0] / 10;
+		else
+			$pages = (int)($row[0] / 10 + 1);
+		if($_GET["current"])
+			$current = $_GET["current"];
+		else $current = 1;
+		if($current <= $pages && $current > 1)
+			echo "<a href='index-1.php?category=" . $category . "&current=" . $current - 1 . "'>&lt;preview</a>";
+		for($i = 1; $i <= $pages; $i ++){
+			if($i == $current)
+				echo "<a href='javascript:void(0);' style='color:gray;'>" . $i . "</a>";
+			else
+				echo "<a href='index-1.php?category=" . $category . "&current=" . $i . "'>" . $i . "</a>";
+		}
+		if($current < $pages && $current >= $pages - 1)
+			echo "<a href='index-1.php?category=" . $category . "&current=" . $current + 1 . "'>next&gt;</a>";
 		?>
 	</div>
 </body>
